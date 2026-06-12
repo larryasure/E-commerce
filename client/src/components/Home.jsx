c0230cdbe91e5d4a6d393e12943deaae8e98807e
Larryasure <olascolanre@gmail.com>
2026-06-12 08:27:39 +0100

    Home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,25 +1,29 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axiosInstance from "../api/axiosConfig";
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [categories, setCategories] = useState([]);
   const [featuredProducts, setFeaturedProducts] = useState([]);
 
-  useEffect(() => {
-    const fetchFeaturedProducts = async () => {
+  useState(() => {
+    const fetchHomeData = async () => {
       try {
-        setLoading(true);
-        const response = await axiosInstance.get("products/");
-        setFeaturedProducts(response.data.slice(0, 9));
+        const [productsRes, categoriesRes] = await Promise.all([
+          axiosInstance.get("products/"),
+          axiosInstance.get("categories/"),
+        ]);
+        setFeaturedProducts(productsRes.data.slice(0, 9));
+        setCategories(categoriesRes.data);
       } catch (error) {
-        console.error("Failed to load products", error);
+        console.error("Failed to load Homepage data", error);
       } finally {
         setLoading(false);
       }
     };
-    fetchFeaturedProducts();
+
+    fetchHomeData();
   }, []);
 
-  
   return <div></div>;
 }
