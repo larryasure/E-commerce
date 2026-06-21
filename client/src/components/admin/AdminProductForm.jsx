6bd87ede8eabec3baf1f876b52bee02cb7bfbf00
Larryasure <olascolanre@gmail.com>
2026-06-21 15:17:59 +0100

    AdminProduct

--- client/src/components/admin/AdminProductForm.jsx
+++ client/src/components/admin/AdminProductForm.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axiosInstance from "../../api/axiosConfig";
 
 export default function AdminProductForm() {
@@ -54,15 +54,17 @@ export default function AdminProductForm() {
     if (errors[name]) {
       setErrors((prev) => ({ ...prev, [name]: "" }));
     }
-    
   };
 
   const validateForm = () => {
     const newErrors = {};
     if (!formData.name.trim()) newErrors.name = "Product name is required";
     if (!formData.slug.trim()) newErrors.slug = "Slug is required";
+    if (!formData.stock.trim()) newErrors.stock = "Stock is required";
     if (!formData.price) newErrors.price = "Price is required";
     if (!formData.category_id) newErrors.category_id = "Category is required";
+    if (!formData.description)
+      newErrors.description = "Description is required";
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
   };
@@ -103,6 +105,179 @@ export default function AdminProductForm() {
   };
 
   return (
-  
+    <>
+      <div className="max-w-md mx-auto">
+        <h1 className="text-[#13315c] text-3xl font-bold mb-5 ">
+          {id ? "Edit Products" : "Add new Products"}
+        </h1>
+
+        <form
+          onSubmit={handleSubmit}
+          className="space-y-3 bg-white rounded-xl shadow-xl p-4"
+        >
+          <div>
+            <label className="font-semibold mb-2 text-[#155daf] text-sm ">
+              Product Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className={`w-full px-4 py-2 focus:ring focus:ring-[#13315c] focus:outline-none border border-[#13315c] rounded-xl transition-all duration-300 ${errors.name ? "border-red-500 focus:ring-red-500" : "border-[#155daf] focus:ring-[#155daf] "}`}
+            />
+
+            {errors.name && (
+              <span className="text-red-500 text-xs  mt-1">{errors.name}</span>
+            )}
+          </div>
+
+          <div>
+            <label className="text-sm text-[#155daf] font-semibold mb-2 ">
+              Slug
+            </label>
+
+            <input
+              type="text"
+              value={formData.slug}
+              onChange={handleChange}
+              name="slug"
+              className={`w-full px-4 py-2 focus:ring focus:ring-[#13315c] focus:outline-none border border-[#13315c] rounded-xl transition-all duration-300 ${errors.slug ? "border-red-500 focus:ring-red-500" : "border-[#155daf] focus:ring-[#155daf] "}`}
+            />
+            {errors.slug && (
+              <span className="text-xs text-red-500 mt-1">{errors.slug}</span>
+            )}
+          </div>
+
+          <div>
+            <label className="block text-sm text-[#155daf] font-semibold">
+              Category
+            </label>
+
+            <select
+              name="category_id"
+              value={formData.category_id}
+              onChange={handleChange}
+              className={`w-full px-4 py-2 border border-[#13315c] focus:ring focus:outline-none transition-all duration-300 focus:ring-[#13315c] rounded-xl ${errors.category_id ? "border-red-500 focus:ring-offset-red-500" : "focus:ring-[#155daf] border-[#155daf]"} `}
+            >
+              <option value="" disabled>
+                Select a Category
+              </option>
+              {categories.map((category) => (
+                <option
+                  className="text-[#13315c]"
+                  value={category.id}
+                  key={category.id}
+                >
+                  {category.name}
+                </option>
+              ))}
+            </select>
+            {errors.category_id && (
+              <span className="text-xs text-red-500 mt-1">
+                {errors.category_id}
+              </span>
+            )}
+          </div>
+
+          <div className="flex flex-col gap-2 max-w-xs">
+            <label className="text-sm font-semibold text-[#155daf]">
+              Image
+            </label>
+
+            <input
+              type="file"
+              className="file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-[#155daf]/10 file:text-[#155daf] hover:file:bg-[#155daf]/20 text-sm text-gray-500 cursor-pointer"
+            />
+          </div>
+
+          <div>
+            <label className="block text-[#155daf] font-semibold mb-2">
+              Price
+            </label>
+            <input
+              type="number"
+              name="price"
+              value={formData.price}
+              onChange={handleChange}
+              step="0.01"
+              className={`w-full px-4 py-2 border border-[#13315c] focus:ring focus:outline-none transition-all duration-300 focus:ring-[#13315c] rounded-xl ${errors.category_id ? "border-red-500 focus:ring-offset-red-500" : "focus:ring-[#155daf] border-[#155daf]"} `}
+            />
+            {errors.price && (
+              <p className="text-red-500 text-sm mt-1">{errors.price}</p>
+            )}
+          </div>
+
+          <div>
+            <label className="block text-[#155daf] font-semibold mb-2">
+              Stock
+            </label>
+            <input
+              type="number"
+              name="stock"
+              value={formData.stock}
+              onChange={handleChange}
+              className={`w-full px-4 py-2 border border-[#13315c] focus:ring focus:outline-none transition-all duration-300 focus:ring-[#13315c] rounded-xl ${errors.stock ? "border-red-500 focus:ring-offset-red-500" : "focus:ring-[#155daf] border-[#155daf]"} `}
+            />
+            {
+              errors.stock && (
+                <span className="text-xs text-red-500 mt-1 ">{ errors.stock}</span>
+              )
+            }
+          </div>
+
+          {/* Description */}
+          <div>
+            <label className="block text-[#155daf] font-semibold mb-2">
+              Description
+            </label>
+            <textarea
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+              rows="4"
+              className={`w-full px-4 py-2 border border-[#13315c] focus:ring focus:outline-none transition-all duration-300 focus:ring-[#13315c] rounded-xl ${errors.description ? "border-red-500 focus:ring-offset-red-500" : "focus:ring-[#155daf] border-[#155daf]"} `}
+            />
+            {errors.description && (
+              <span className="text-xs mt-1 text-red-500">
+                {errors.description}
+              </span>
+            )}
+          </div>
+
+          {/* Active */}
+          <div className="flex items-center gap-3">
+            <input
+              type="checkbox"
+              name="is_active"
+              checked={formData.is_active}
+              onChange={handleChange}
+              className="w-4 h-4 text-[#155daf]"
+            />
+            <label className="text-[#13315C] text-sm font-semibold">
+              Active
+            </label>
+          </div>
+
+          {/* Submit */}
+          <div className="flex gap-4 pt-6">
+            <button
+              type="submit"
+              disabled={loading}
+              className="flex-1 bg-[#155daf] text-white py-3 rounded-lg hover:bg-[#13315C] disabled:bg-gray-400 font-semibold transition-colors"
+            >
+              {loading ? "Saving..." : id ? "Update Product" : "Create Product"}
+            </button>
+            <button
+              type="button"
+              onClick={() => navigate("/admin/products")}
+              className="flex-1 border-2 border-[#155daf] text-[#155daf] py-2 rounded-lg hover:bg-[#155daf] hover:text-white font-semibold transition-colors"
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
